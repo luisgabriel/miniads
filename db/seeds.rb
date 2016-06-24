6b773975300778e3f74fc2bf24299718fdabf479
Luís Gabriel Lima <lampih@gmail.com>
2016-06-24 20:38:30 -0300

    Add database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,58 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ad_list = [
+  [
+    243.4,
+    [
+      [ 12.6, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' ],
+      [ 137.6, 'Aenean at felis porttitor, pulvinar mauris ac, laoreet est.' ]
+    ],
+    [
+      [ 'Austin, London, Toronto, Santiago', :unkown ],
+      [ '', :male ]
+    ]
+  ],
+  [
+    1344.8,
+    [
+      [ 667.6, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' ],
+      [ 34.6, 'Aenean at felis porttitor, pulvinar mauris ac, laoreet est.' ],
+      [ 27.46, 'Aenean at felis porttitor, pulvinar mauris ac, laoreet est.' ]
+    ],
+    [
+      [ 'Austin, London, Toronto, Santiago', :unkown ],
+      [ 'Recife', :female ],
+      [ '', :male ],
+      [ 'Warsaw, Moscow, Talin', :male ],
+      [ 'Olinda, Campinas', :unkown ]
+    ]
+  ],
+  [
+    434.7,
+    [
+      [ 57.6, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' ]
+    ],
+    [
+      [ 'Austin, London, Toronto, Santiago', :unkown ],
+      [ 'Recife', :female ],
+      [ '', :male ],
+      [ 'Warsaw, Moscow, Talin', :male ],
+      [ 'Olinda, Campinas', :unkown ]
+    ]
+  ]
+]
+
+
+for i in 1..4
+  ad_list.each do |budget, creatives, targetings|
+    temp = Ad.new budget: budget
+
+    creatives.each do |bid, ad_text|
+      temp.creatives.new bid: bid, ad_text: ad_text
+    end
+
+    targetings.each do |places, gender|
+      temp.targetings.new places: places, gender: gender
+    end
+
+    temp.save!
+  end
+end
